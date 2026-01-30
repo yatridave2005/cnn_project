9c15cf7d47081266be0c73f600df15277e7bfa0f
yatridave2005 <jagrutidave789@gmail.com>
2026-01-30 18:54:00 +0530

    Add input and output details retrieval for interpreter
    Retrieve input and output details from the TensorFlow Lite interpreter.

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import tensorflow as tf
 
 interpreter = tf.lite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 
 
 
